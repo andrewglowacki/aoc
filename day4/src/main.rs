f3292a0acafad101c1837a832016ed389741d1a6
andrewglowacki <andrewglowacki@live.com>
2020-12-05 13:15:55 -0500

    refactor day4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -4,7 +4,21 @@ use std::io::{BufRead, BufReader};
 use std::collections::HashMap;
 use regex::Regex;
 
-const HAS_ALL: i32 = 127;
+const HAS_ALL: u32 = 127;
+
+type Mappings = HashMap<&'static str, Mapping>;
+
+#[derive(Debug, Clone)]
+struct Mapping {
+    field: &'static str,
+    flag: u32,
+    validator: Regex
+}
+
+struct FieldValue {
+    mapping: Mapping,
+    value: String
+}
 
 fn main() {
     let path = Path::new("input.txt");
@@ -13,33 +27,22 @@ fn main() {
     let mut valid: i32 = 0;
     let mut lines = BufReader::new(file).lines();
     
-    let mapping = vec![
-        ("cid", 0),
-        ("iyr", 1),
-        ("byr", 2),
-        ("eyr", 4),
-        ("hgt", 8),
-        ("hcl", 16),
-        ("ecl", 32),
-        ("pid", 64)
+    let default_mapping: Mapping = Mapping { field: "cid", flag: 0, validator: Regex::new(r".*").unwrap() };
+
+    let mappings: Vec<Mapping> = vec![
+        default_mapping,
+        Mapping { field: "iyr", flag: 1, validator: Regex::new(r"^20(1[0-9]|20)$").unwrap() },
+        Mapping { field: "byr", flag: 2, validator: Regex::new(r"^(19[2-9][0-9]|200[0-2])$").unwrap() },
+        Mapping { field: "eyr", flag: 4, validator: Regex::new(r"^20(2[0-9]|30)$").unwrap() },
+        Mapping { field: "hgt", flag: 8, validator: Regex::new(r"^(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)$").unwrap() },
+        Mapping { field: "hcl", flag: 16, validator: Regex::new(r"^#[0-9a-f]{6}$").unwrap() },
+        Mapping { field: "ecl", flag: 32, validator: Regex::new(r"^(amb|blu|brn|gry|grn|hzl|oth)$").unwrap() },
+        Mapping { field: "pid", flag: 64, validator: Regex::new(r"^[0-9]{9}$").unwrap() },
     ];
 
-    let token_map: HashMap<String, i32> = mapping.into_iter()
-        .map(|entry| (String::from(entry.0), entry.1))
+    let token_map: Mappings = mappings.into_iter()
+        .map(|mapping| (mapping.field, mapping))
         .collect();
-
-    let valid_mapping: Vec<(i32, Regex)> = vec![
-        (0, Regex::new(r".*").unwrap()), // cid/none
-        (1, Regex::new(r"^20(1[0-9]|20)$").unwrap()), // iyr
-        (2, Regex::new(r"^(19[2-9][0-9]|200[0-2])$").unwrap()), // byr
-        (4, Regex::new(r"^20(2[0-9]|30)$").unwrap()), // eyr
-        (8, Regex::new(r"^(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)$").unwrap()), // hgt
-        (16, Regex::new(r"^#[0-9a-f]{6}$").unwrap()), // hcl
-        (32, Regex::new(r"^(amb|blu|brn|gry|grn|hzl|oth)$").unwrap()), // ecl
-        (64, Regex::new(r"^[0-9]{9}$").unwrap()), // pid
-    ];
-    
-    let valid_checks: HashMap<i32, Regex> = valid_mapping.into_iter().collect();
 
     let mut all = 0;
     while let Some(Ok(line)) = lines.next() {
@@ -53,9 +56,20 @@ fn main() {
 
         all |= line.split(char::is_whitespace)
             .map(|token| token.split(':').collect::<Vec<&str>>() )
-            .map(|token| (token_map.get(token[0]).unwrap_or(&0), token[1]))
-            .filter(|entry| valid_checks.get(entry.0).unwrap().is_match(entry.1))
-            .map(|entry| entry.0)
+            .map(|token| {
+                if let Some(mapping) = token_map.get(token[0]) {
+                    Some(FieldValue {
+                        mapping: mapping.clone(),
+                        value: String::from(token[1])
+                    })
+                } else {
+                    None
+                }
+            })
+            .filter(|entry| !entry.is_none())
+            .map(|entry| entry.unwrap())
+            .filter(|entry| entry.mapping.validator.is_match(entry.value.as_str()))
+            .map(|entry| entry.mapping.flag)
             .fold(0, |sum, val| sum | val);
         
     }
