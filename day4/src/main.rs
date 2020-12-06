387e0c0aa173960cf7688804809d7e1e6f01952a
andrewglowacki <andrewglowacki@live.com>
2020-12-06 16:41:12 -0500

    more refactor day4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,7 +1,8 @@
 use std::path::Path;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Lines};
 use std::collections::HashMap;
+use std::iter::Peekable;
 use regex::Regex;
 
 const HAS_ALL: u32 = 127;
@@ -15,9 +16,45 @@ struct Mapping {
     validator: Regex
 }
 
+impl Mapping {
+    fn new(field: &'static str, flag: u32, regex: &str) -> Mapping {
+        Mapping {
+            field,
+            flag, 
+            validator: Regex::new(regex).unwrap()
+        }
+    }
+}
+
 struct FieldValue {
     mapping: Mapping,
     value: String
+}
+
+fn split_copy(line: String, split_char: char) -> Vec<String> {
+    line.split(split_char)
+        .map(|s| String::from(s))
+        .collect::<Vec<String>>()
+}
+
+fn is_next_passport_valid(token_map: &Mappings, lines: &mut Peekable<Lines<BufReader<File>>>) -> bool {
+    lines.filter_map(|line| line.ok())
+        .take_while(|line| !line.is_empty())
+        .flat_map(|line| split_copy(line, ' '))
+        .map(|token| split_copy(token, ' '))
+        .filter_map(|token| {
+            if let Some(mapping) = token_map.get(token[0].as_str()) {
+                Some(FieldValue {
+                    mapping: mapping.clone(),
+                    value: String::from(token[1].as_str())
+                })
+            } else {
+                None
+            }
+        })
+        .filter(|entry| entry.mapping.validator.is_match(entry.value.as_str()))
+        .map(|entry| entry.mapping.flag)
+        .fold(0, |sum, val| sum | val) == HAS_ALL
 }
 
 fn main() {
@@ -25,56 +62,27 @@ fn main() {
     let file = File::open(path).unwrap();
 
     let mut valid: i32 = 0;
-    let mut lines = BufReader::new(file).lines();
+    let mut lines = BufReader::new(file).lines().peekable();
     
-    let default_mapping: Mapping = Mapping { field: "cid", flag: 0, validator: Regex::new(r".*").unwrap() };
-
     let mappings: Vec<Mapping> = vec![
-        default_mapping,
-        Mapping { field: "iyr", flag: 1, validator: Regex::new(r"^20(1[0-9]|20)$").unwrap() },
-        Mapping { field: "byr", flag: 2, validator: Regex::new(r"^(19[2-9][0-9]|200[0-2])$").unwrap() },
-        Mapping { field: "eyr", flag: 4, validator: Regex::new(r"^20(2[0-9]|30)$").unwrap() },
-        Mapping { field: "hgt", flag: 8, validator: Regex::new(r"^(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)$").unwrap() },
-        Mapping { field: "hcl", flag: 16, validator: Regex::new(r"^#[0-9a-f]{6}$").unwrap() },
-        Mapping { field: "ecl", flag: 32, validator: Regex::new(r"^(amb|blu|brn|gry|grn|hzl|oth)$").unwrap() },
-        Mapping { field: "pid", flag: 64, validator: Regex::new(r"^[0-9]{9}$").unwrap() },
+        Mapping::new("cid", 0, r".*"),
+        Mapping::new("iyr", 1, r"^20(1[0-9]|20)$"),
+        Mapping::new("byr", 2, r"^(19[2-9][0-9]|200[0-2])$"),
+        Mapping::new("eyr", 4, r"^20(2[0-9]|30)$"),
+        Mapping::new("hgt", 8, r"^(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)$"),
+        Mapping::new("hcl", 16, r"^#[0-9a-f]{6}$"),
+        Mapping::new("ecl", 32, r"^(amb|blu|brn|gry|grn|hzl|oth)$"),
+        Mapping::new("pid", 64, r"^[0-9]{9}$"),
     ];
 
     let token_map: Mappings = mappings.into_iter()
         .map(|mapping| (mapping.field, mapping))
         .collect();
 
-    let mut all = 0;
-    while let Some(Ok(line)) = lines.next() {
-        if line.is_empty() {
-            if all == HAS_ALL {
-                valid += 1;
-            }
-            all = 0;
-            continue;
+    while lines.peek().is_some() {
+        if is_next_passport_valid(&token_map, &mut lines) {
+            valid += 1;
         }
-
-        all |= line.split(char::is_whitespace)
-            .map(|token| token.split(':').collect::<Vec<&str>>() )
-            .map(|token| {
-                if let Some(mapping) = token_map.get(token[0]) {
-                    Some(FieldValue {
-                        mapping: mapping.clone(),
-                        value: String::from(token[1])
-                    })
-                } else {
-                    None
-                }
-            })
-            .filter(|entry| !entry.is_none())
-            .map(|entry| entry.unwrap())
-            .filter(|entry| entry.mapping.validator.is_match(entry.value.as_str()))
-            .map(|entry| entry.mapping.flag)
-            .fold(0, |sum, val| sum | val);
-        
-    }
-    if all == HAS_ALL {
-        valid += 1;
     }
 
     println!("Valid: {}", valid);
