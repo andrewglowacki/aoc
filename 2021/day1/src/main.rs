82253f42b9a022db00ab5209b9cb0da6b6db5183
andrewglowacki <andrewglowacki>
2021-12-02 01:43:48 -0500

    day1

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -1,35 +1,64 @@
 use std::fs::File;
 use std::path::Path;
-use std::io::{self, BufRead};
-use std::collections::BTreeSet;
-use std::ops::Bound::Excluded;
+use std::io::{BufRead, BufReader, Lines};
+
+type Input = Lines<BufReader<File>>;
+
+fn get_file_lines(file_name: &str) -> Input {
+    let path = Path::new(file_name);
+    let file = File::open(path).unwrap();
+    BufReader::new(file).lines()
+}
+
+fn part_one(file_name: &str) {
+    let numbers = get_file_lines(file_name)
+        .flat_map(|line| line.ok())
+        .flat_map(|line| line.parse::<i32>())
+        .collect::<Vec<i32>>();
+    
+    let mut last = numbers[0];
+    let mut increases = 0;
+    for number in numbers {
+        if number > last {
+            increases += 1;
+        }
+        last = number;
+    }
+
+    println!("Part 1: Increases: {}", increases);
+}
+
+fn part_two(file_name: &str) {
+    let mut numbers = get_file_lines(file_name)
+        .flat_map(|line| line.ok())
+        .flat_map(|line| line.parse::<i32>());
+    
+    let mut queue = vec![
+        numbers.next().unwrap(), 
+        numbers.next().unwrap(), 
+        numbers.next().unwrap()
+    ];
+
+    let mut increases = 0;
+    let mut last = queue.iter().sum::<i32>();
+    let mut insert = 0;
+    
+    numbers.for_each(|num| {
+        let new_sum = last - queue[insert] + num;
+        queue[insert] = num;
+        insert = (insert + 1) % 3;
+        if new_sum > last {
+            increases += 1;
+        }
+        last = new_sum;
+    });
+
+    println!("Part 2: Increases: {}", increases);
+}
 
 fn main() {
-    let path = Path::new("input.txt");
-    let file = File::open(path).unwrap();
-
-    let mut seen: BTreeSet<i32> = BTreeSet::new();
-
-    for line in io::BufReader::new(file).lines() {
-        let number = line.unwrap().parse::<i32>().unwrap();
-        let diff = 2020 - number;
-        let range = {
-            if diff > number {
-                (Excluded(number), Excluded(diff))
-            } else {
-                (Excluded(diff), Excluded(number))
-            }
-        };
-
-        for check in seen.range(range) {
-            let left = diff - check;
-            if seen.contains(&left) {
-                println!("{} * {} * {} = {}", number, check, left, (number * check * left));
-                return;
-            }
-        }
-        seen.insert(number);
-    }
+    part_one("input.txt");
+    part_two("input.txt");
 
     println!("Done!");
 }
