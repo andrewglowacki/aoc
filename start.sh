f045bb3d283030d1db58899acb1ff3821c79a888
andrewglowacki <andrewglowacki>
2021-12-04 16:28:15 -0500

    Add java to start template

--- start.sh
+++ start.sh
@@ -9,6 +9,8 @@ fi
 
 num=$1
 cp -R $DIR/dayX day$num
-sed -i "s/dayX/day${num}/g" day${num}/{Cargo.toml,Cargo.lock}
+sed -i "s/dayX/day${num}/g" day${num}/{Cargo.toml,Cargo.lock,pom.xml}
+sed -i "s/DayX/Day${num}/g" day${num}/src/main/java/ag/aoc/DayX.java
+mv day${num}/src/main/java/ag/aoc/DayX.java day${num}/src/main/java/ag/aoc/Day${num}.java
 AOC_COOKIE=`cat ~/aoc-cookie`
 curl -b "session=$AOC_COOKIE" https://adventofcode.com/2021/day/${num}/input > day${num}/input.txt
