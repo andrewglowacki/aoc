46d2eee825f1ae86cef16ceb5f6c662316496f25
andrewglowacki <andrewglowacki>
2021-12-29 16:42:38 -0500

    day15 final

--- 2021/day15/src/main.rs
+++ 2021/day15/src/main.rs
@@ -1,5 +1,4 @@
-use std::collections::HashMap;
-use std::cmp::min;
+use std::collections::BTreeSet;
 use std::collections::HashSet;
 use std::fs::File;
 use std::path::Path;
@@ -23,70 +22,22 @@ struct Cave {
 
 struct CaveLog {
     visiting: HashSet<(usize, usize)>,
-    calculated: HashMap<(usize, usize, u8), u32>
+    visitable: BTreeSet<(u32, usize, usize)>
 }
 
 impl CaveLog {
     fn new() -> CaveLog {
         CaveLog {
             visiting: HashSet::new(),
-            calculated: HashMap::new()
+            visitable: BTreeSet::new()
         }
     }
-    fn visited(&mut self, r: usize, c: usize, risk: u32, config: u8) {
-        self.visiting.remove(&(r, c));
-        self.calculated.insert((r, c, config), risk);
-    }
-    fn get_risk(&self, r: usize, c: usize, config: u8) -> Option<&u32> {
-        self.calculated.get(&(r, c, config))
-    }
-}
-
-struct Stack {
-    frames: Vec<Frame>
-}
-
-impl Stack {
-    fn new() -> Stack {
-        Stack {
-            frames: Vec::new()
-        }
-    }
-    fn push(&mut self, frame: Frame) {
-        self.frames.push(frame);
-    }
-    fn pop(&mut self) -> Frame {
-        self.frames.pop().unwrap()
-    }
-    fn last(&mut self) -> &mut Frame {
-        let len = self.frames.len();
-        self.frames.get_mut(len - 1).unwrap()
-    }
-    fn has_frames(&self) -> bool {
-        self.frames.len() > 0
-    }
-}
-
-struct Frame {
-    r: usize,
-    c: usize,
-    config: u8,
-    points: Vec<(usize, usize)>,
-    lowest_risk: u32,
-}
-
-impl Frame {
-    fn new(r: usize, c: usize, config: u8, points: Vec<(usize, usize)>) -> Frame {
-        Frame {
-            r,
-            c,
-            config,
-            points,
-            lowest_risk: u32::MAX
-        }
-    }
-    fn check_risk(&mut self, risk: u32) {
-        self.lowest_risk = min(self.lowest_risk, risk);
+    fn visit_least(&mut self) -> (u32, usize, usize) {
+        let first = *self.visitable.iter().next().unwrap();
+        self.visitable.remove(&first);
+        let (_, r, c) = first;
+        self.visiting.insert((r, c));
+        first
     }
 }
 
@@ -120,147 +71,52 @@ impl Cave {
         }
     }
 
-    fn add_if_valid(&self, r: i32, c: i32, log: &CaveLog, points: &mut Vec<(usize, usize)>) -> bool {
+    fn add_if_valid(&self, r: i32, c: i32, current_risk: u32, log: &mut CaveLog) {
         if r >= 0 && r < self.height as i32 && c >= 0 && c < self.width as i32 {
             let point = (r as usize, c as usize);
             if !log.visiting.contains(&point) {
-                points.push(point);
-                return true;
+                let risk = self.get_risk(point.0, point.1) + current_risk;
+                log.visitable.insert((risk, point.0, point.1));
             }
         }
-        false
     }
     
-    fn get_adjacent_points(&self, r: usize, c: usize, log: &CaveLog) -> (u8, Vec<(usize, usize)>) {
-        let mut points = vec![];
-        let mut config = 0;
+    fn add_adjacent_points(&self, r: usize, c: usize, current_risk: u32, log: &mut CaveLog) {
         let c = c as i32;
         let r = r as i32;
-        if self.add_if_valid(r - 1, c, log, &mut points) {
-            config = 0x01;
-        }
-        if self.add_if_valid(r + 1, c, log, &mut points) {
-            config |= 0x02;
-        }
-        if self.add_if_valid(r, c - 1, log, &mut points) {
-            config |= 0x04;
-        }
-        if self.add_if_valid(r, c + 1, log, &mut points) {
-            config |= 0x08;
-        }
-        (config, points)
+        self.add_if_valid(r - 1, c, current_risk, log);
+        self.add_if_valid(r + 1, c, current_risk, log);
+        self.add_if_valid(r, c - 1, current_risk, log);
+        self.add_if_valid(r, c + 1, current_risk, log);
     }
 
-    fn find_least_risky_path(&self) -> (u32, HashSet<(usize, usize)>) {
+    fn find_least_risky_path(&self) -> u32 {
         let mut log = CaveLog::new();
-        let mut stack = Stack::new();
-        let (config, points) = self.get_adjacent_points(0, 0, &log);
-        stack.push(Frame::new(0, 0, config, points));
+
+        log.visiting.insert((0, 0));
+        self.add_adjacent_points(0, 0, 0, &mut log);
+
         loop {
-            let mut frame = stack.pop();
-
-            let r = frame.r;
-            let c = frame.c;
-            let config = frame.config;
-            
-            let base_risk = self.get_risk(r, c);
-            
-            if r == self.height - 1 && c == self.width - 1 {
-                // end game
-                let prev = stack.last();
-                prev.check_risk(base_risk);
-                continue;
-            } else if let Some(risk_to_end) = log.get_risk(r, c, config) {
-                if stack.has_frames() {
-                    let prev = stack.last();
-                    prev.check_risk(*risk_to_end);
-                    continue;
-                } else {
-                    return (*risk_to_end, self.get_path_to_end(r, c, &mut log));
-                }
+            let (risk, r, c) = log.visit_least();
+            if r == self.width - 1 && c == self.height - 1 {
+                return risk;
             }
-
-            log.visiting.insert((r, c));
-
-            if frame.points.len() > 0 {
-                let (r, c) = frame.points.pop().unwrap();
-                let (new_config, new_points) = self.get_adjacent_points(r, c, &log);
-                stack.push(frame);
-                if new_points.len() > 0 {
-                    stack.push(Frame::new(r, c, new_config, new_points));
-                }
-                continue;
-            }
-
-            if frame.lowest_risk == u32::MAX {
-                log.visiting.remove(&(r, c));
-            } else {
-                let result = frame.lowest_risk + base_risk;
-                log.visited(r, c, result, config);
-                
-                if stack.has_frames() {
-                    let prev = stack.last();
-                    prev.check_risk(result);
-                } else {
-                    return (result, self.get_path_to_end(r, c, &mut log));
-                }
-            }
+            self.add_adjacent_points(r, c, risk, &mut log);
         }
-    }
 
-    fn get_path_to_end(&self, r: usize, c: usize, log: &mut CaveLog) -> HashSet<(usize, usize)> {
-        if r == self.height - 1 && c == self.width - 1 {
-            log.visiting.clone()
-        } else {
-            log.visiting.insert((r, c));
-            let (_, points) = self.get_adjacent_points(r, c, &log);
-            if let Some((r, c, _)) = points.into_iter()
-                .flat_map(|(r, c)| {
-                    let (config, _) = self.get_adjacent_points(r, c, &log);
-                    if let Some(risk) = log.get_risk(r, c, config) {
-                        Some((r, c, *risk))
-                    } else if r == self.height - 1 && c == self.width - 1 {
-                        Some((r, c, self.get_risk(r, c)))
-                    } else {
-                        None
-                    }
-                })
-                .min_by_key(|(_, _, risk)| *risk) 
-            {
-                self.get_path_to_end(r, c, log)
-            }
-            else
-            {
-                panic!("at ({}, {}) no next point with {} visited thus far", r, c, log.visiting.len());
-            }
-        }
-    }
-
-    fn _print_path(&self, points: HashSet<(usize, usize)>) {
-        for r in 0..self.height {
-            for c in 0..self.width {
-                let in_path = points.contains(&(r, c));
-                if in_path {
-                    print!("\x1b[42m{}\x1b[0m", self.get_risk(r, c));
-                } else {
-                    print!("{}", self.get_risk(r, c));
-                }
-            }
-            println!("");
-        }
     }
 
 }
 
 fn part_one(file_name: &str) {
     let cave = Cave::from_file(file_name, 1);
-    let (risk, _) = cave.find_least_risky_path();
+    let risk = cave.find_least_risky_path();
     println!("Part 1: {}", risk);
 }
 
 fn part_two(file_name: &str) {
     let cave = Cave::from_file(file_name, 5);
-    let (risk, _) = cave.find_least_risky_path();
+    let risk = cave.find_least_risky_path();
     println!("Part 2: {}", risk);
 }
 
