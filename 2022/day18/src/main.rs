df33278704ec953c1292af1a7fe7fa22dc9bc61c
andrewglowacki <andrewglowacki>
2022-12-19 19:32:20 -0500

    day18

--- 2022/day18/src/main.rs
+++ 2022/day18/src/main.rs
@@ -72,27 +72,19 @@ fn part_one(file_name: &str) {
     println!("Part 1: {}", total);
 }
 
-fn add_point_to_map(point: (i32, i32), third: i32, map: &mut HashMap<(i32, i32), BTreeSet<i32>>) {
-    if let Some(set) = map.get_mut(&point) {
-        set.insert(third);
-    } else {
-        let mut set = BTreeSet::new();
-        set.insert(third);
-        map.insert(point, set);
-    }
-}
-
+#[derive(Debug)]
 enum Type {
     Rock,
-    Unknown,
-    Interior,
     Exterior
 }
 
 struct Droplet {
-    x_range: Range<i32>,
-    y_range: Range<i32>,
-    z_range: Range<i32>,
+    x_min: i32,
+    x_max: i32, 
+    y_min: i32,
+    y_max: i32,
+    z_min: i32,
+    z_max: i32,
     types: HashMap<(i32, i32, i32), Type>
 }
 
@@ -116,39 +108,183 @@ impl Droplet {
             z_min = z_min.min(*z);
         });
 
-        let x_range = x_min..x_max + 1;
-        let y_range = y_min..y_max + 1;
-        let z_range = z_min..z_max + 1;
-
-        for z in z_range.to_owned() {
-            for y in y_range.to_owned() {
-                for x in x_range.to_owned() {
-                    let point = (x, y, z);
-                    if points.contains(&point) {
-                        types.insert(point, Type::Rock);
-                    } else {
-                        types.insert(point, Type::Unknown);
-                    }
-                }
-            }
-        }
+        points.iter().for_each(|point| {
+            types.insert(*point, Type::Rock);
+        });
 
         Droplet {
-            x_range, 
-            y_range,
-            z_range, 
+            x_min, 
+            x_max,
+            y_min, 
+            y_max,
+            z_min,
+            z_max,
             types
         }
     }
 
-    fn find_interior_points(&mut self, sides: &HashMap<(i32, i32, i32), u32>) {
-        for (point, _) in sides {
-            if self.types.contains_key(point) {
-                continue;
+    fn add_side_types<F>(
+        &mut self, 
+        a_min: i32, 
+        a_max: i32, 
+        b_min: i32, 
+        b_max: i32, 
+        plane_min: i32, 
+        plane_max: i32,
+        point_maker: F) 
+        where F: Fn(i32, i32, i32) -> (i32, i32, i32)
+    {
+        for a in a_min..a_max + 1 {
+            for b in b_min..b_max + 1 {
+                let point = point_maker(a, b, plane_min);
+                if self.types.get(&point).is_none() {
+                    self.types.insert(point, Type::Exterior);
+                }
+                
+                let point = point_maker(a, b, plane_min - 1);
+                self.types.insert(point, Type::Exterior);
+                
+                let point = point_maker(a, b, plane_max);
+                if self.types.get(&point).is_none() {
+                    self.types.insert(point, Type::Exterior);
+                }
+                
+                let point = point_maker(a, b, plane_max + 1);
+                self.types.insert(point, Type::Exterior);
             }
-
-
         }
+    }
+
+    fn is_exterior(&self, point: (i32, i32, i32)) -> bool {
+        match self.types.get(&point) {
+            Some(Type::Exterior) => true,
+            _ => false
+        }
+    }
+
+    fn connect_exteriors<F>(
+        &mut self, 
+        a_min: i32, a_max: i32, 
+        b_min: i32, b_max: i32,
+        c_min: i32, c_max: i32,
+        point_maker: F)
+        where F: Fn(i32, i32, i32) -> (i32, i32, i32)
+    {
+        for a in a_min..a_max + 1 {
+            for b in b_min..b_max + 1 {
+                for c in c_min..c_max + 1 {
+                    let has_adjacent_exterior =
+                        self.is_exterior(point_maker(a - 1, b, c)) ||
+                        self.is_exterior(point_maker(a + 1, b, c)) ||
+                        self.is_exterior(point_maker(a, b - 1, c)) ||
+                        self.is_exterior(point_maker(a, b + 1, c)) ||
+                        self.is_exterior(point_maker(a, b, c - 1)) ||
+                        self.is_exterior(point_maker(a, b, c + 1));
+                        
+                    if has_adjacent_exterior {
+                        let point = point_maker(a, b, c);
+                        if !self.types.contains_key(&point) {
+                            self.types.insert(point, Type::Exterior);
+                        }
+                    }                    
+                }
+            }
+        }
+        
+        for a in a_min..a_max + 1 {
+            let a = a_max - a;
+            for b in b_min..b_max + 1 {
+                let b = b_max - b;
+                for c in c_min..c_max + 1 {
+                    let c = c_max - c;
+                    let has_adjacent_exterior =
+                        self.is_exterior(point_maker(a - 1, b, c)) ||
+                        self.is_exterior(point_maker(a + 1, b, c)) ||
+                        self.is_exterior(point_maker(a, b - 1, c)) ||
+                        self.is_exterior(point_maker(a, b + 1, c)) ||
+                        self.is_exterior(point_maker(a, b, c - 1)) ||
+                        self.is_exterior(point_maker(a, b, c + 1));
+                        
+                    if has_adjacent_exterior {
+                        let point = point_maker(a, b, c);
+                        if !self.types.contains_key(&point) {
+                            self.types.insert(point, Type::Exterior);
+                        }
+                    }                    
+                }
+            }
+        }
+    }
+
+    fn count_exterior_points(&mut self, sides: &HashMap<(i32, i32, i32), u32>) -> u32 {
+        //
+        // mark boundaries
+        //
+        
+        // top and bottom
+        self.add_side_types(
+            self.x_min, self.x_max, 
+            self.y_min, self.y_max, 
+            self.z_min, self.z_max, 
+            |x, y, z| (x, y, z));
+        
+        // left and right
+        self.add_side_types(
+            self.x_min, self.x_max, 
+            self.z_min, self.z_max, 
+            self.y_min, self.y_max, 
+            |x, z, y| (x, y, z));
+            
+        // front and back
+        self.add_side_types(
+            self.z_min, self.z_max, 
+            self.y_min, self.y_max, 
+            self.x_min, self.x_max, 
+            |z, y, x| (x, y, z));
+      
+        // find unmarked spaces adjacent to exteriors
+        // these are also exteriors
+        self.connect_exteriors(
+            self.x_min, self.x_max, 
+            self.y_min, self.y_max, 
+            self.z_min, self.z_max, 
+            |x, y, z| (x, y, z));
+            
+        self.connect_exteriors(
+            self.x_min, self.x_max, 
+            self.z_min, self.z_max, 
+            self.y_min, self.y_max, 
+            |x, z, y| (x, y, z));
+            
+        self.connect_exteriors(
+            self.z_min, self.z_max, 
+            self.y_min, self.y_max, 
+            self.x_min, self.x_max, 
+            |z, y, x| (x, y, z));
+            
+        self.connect_exteriors(
+            self.z_min, self.z_max, 
+            self.x_min, self.x_max, 
+            self.y_min, self.y_max, 
+            |z, y, x| (x, y, z));
+
+        self.connect_exteriors(
+            self.y_min, self.y_max, 
+            self.x_min, self.x_max, 
+            self.z_min, self.z_max, 
+            |y, x, z| (x, y, z));
+
+        self.connect_exteriors(
+            self.y_min, self.y_max, 
+            self.z_min, self.z_max, 
+            self.x_min, self.x_max, 
+            |y, x, z| (x, y, z));
+
+        // filter the sides down to only exteriors and return the sum
+        sides.iter()
+            .filter(|(point, _)| self.is_exterior(**point))
+            .map(|(_, count)| count)
+            .sum()
     }
 }
 
@@ -164,10 +300,26 @@ fn part_two(file_name: &str) {
 
     let mut droplet = Droplet::create(&points);
 
-    droplet.find_interior_points(&sides);
-    
-    let total = sides.values().sum::<u32>();
+    let total = droplet.count_exterior_points(&sides);
 
+    for z in droplet.z_min - 1..droplet.z_max + 2 {
+        println!("----- Z {} -----", z);
+        for y in droplet.y_min - 1..droplet.y_max + 2 {
+            for x in droplet.x_min - 1..droplet.x_max + 2 {
+                if let Some(t) = droplet.types.get(&(x, y, z)) {
+                    let symbol = match t {
+                        Type::Rock => '#',
+                        Type::Exterior => '~'
+                    };
+                    print!("{}", symbol);
+                } else {
+                    print!(".");
+                }
+            }
+            println!("");
+        }
+    }
+    
     println!("Part 2: {}", total);
 }
 
