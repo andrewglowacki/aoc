b0abbc4fb7331d402d34f3847662e1e3279a5666
andrewglowacki <andrewglowacki>
2023-12-13 18:44:24 -0500

    day12 pt2

--- 2023/day12/src/main.rs
+++ 2023/day12/src/main.rs
@@ -1,6 +1,11 @@
 use std::fs::File;
 use std::path::Path;
 use std::io::{BufRead, BufReader, Lines};
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+
+use concurrent_queue::ConcurrentQueue;
 
 type Input = Lines<BufReader<File>>;
 
@@ -10,6 +15,7 @@ fn get_file_lines(file_name: &str) -> Input {
     BufReader::new(file).lines()
 }
 
+#[derive(Clone, Debug)]
 struct Report {
     counts: Vec<usize>,
     springs: Vec<char>
@@ -33,6 +39,24 @@ impl Report {
 
         Report { counts, springs }
     }
+    
+    fn unfold(&mut self) {
+        let orig_len = self.springs.len();
+        for _ in 0..4 {
+            self.springs.push('?');
+            for i in 0..orig_len {
+                self.springs.push(self.springs[i]);
+            }
+        }
+
+        let orig_len = self.counts.len();
+        for _ in 0..4 {
+            for i in 0..orig_len {
+                self.counts.push(self.counts[i]);
+            }
+        }
+        
+    }
 
     fn count_options(&self, start: usize, count_index: usize) -> usize {
         let count = self.counts[count_index];
@@ -42,9 +66,6 @@ impl Report {
             (self.counts.len() - (count_index + 1));
 
         let mut options = 0;
-
-        // println!("counting options with start {} count_index: {}, count: {}", start, count_index, count);
-        // println!("right space required: {}", right_space_required);
 
         for i in start..self.springs.len() - right_space_required {
             if i + count > self.springs.len() {
@@ -57,13 +78,9 @@ impl Report {
                 break;
             }
             
-            // println!("checking start {} for count_index {}", i, count_index);
-
             let has_space = self.springs[i..i + count].iter()
                 .any(|c| *c == '.') == false;
             if !has_space {
-                // not possible
-                // println!("not possible starting at {} - no space", i);
                 continue;
             }
 
@@ -74,8 +91,6 @@ impl Report {
                 let sep = self.springs[i + count];
 
                 if sep == '#' {
-                    // not possible
-                    // println!("not possible starting at {} - sep is #", i);
                     continue;
                 }
             }
@@ -83,35 +98,30 @@ impl Report {
             let start_next = i + count + 1;
             
             if count_index + 1 < self.counts.len() {
-                // println!("starting next count index at {}", start_next);
                 options += self.count_options(start_next, count_index + 1);
-                // println!("done with next count index at {}", start_next);
             } else {
                 let has_damaged = self.springs[i + count..].iter()
                     .any(|c| *c == '#');
                 if !has_damaged {
                     options += 1;
-                //     println!("option found");
-                // } else {
-                    // println!("not possible at end - damages are left");
                 }
             }
         }
 
-        // println!("returning options {} for count_index {}", options, count_index);
         options
     }
 
 }
 
 fn part_one(file_name: &str) {
+    let mut index = 0;
     let total = get_file_lines(file_name)
         .flat_map(|line| line.ok())
         .map(|line| Report::parse(line))
         .map(|report| {
-            // println!("testing {:?} - {:?}", report.springs, report.counts);
+            index += 1;
             let count = report.count_options(0, 0);
-            // println!("count for {:?} - {:?} is {}", report.springs, report.counts, count);
+            println!("#{} = {}", index, count);
             count
         })
         .sum::<usize>();
@@ -120,15 +130,58 @@ fn part_one(file_name: &str) {
 }
 
 fn part_two(file_name: &str) {
-    let lines = get_file_lines(file_name)
-        .flat_map(|line| line.ok());
+    let reports = get_file_lines(file_name)
+        .flat_map(|line| line.ok())
+        .map(|line| Report::parse(line))
+        .collect::<Vec<_>>();
+
+    let thread_count = 10;
+    let queue = ConcurrentQueue::<Report>::bounded(reports.len());
     
-    println!("Part 2: {}", "incomplete");
+    reports.into_iter().for_each(|mut report| {
+        report.unfold();
+        queue.push(report).unwrap();
+    });
+
+    queue.close();
+
+    let queue_rc = Arc::new(queue);
+
+    let mut handles = Vec::new();
+
+    for _ in 0..thread_count {
+        let copy = Arc::clone(&queue_rc);
+        handles.push(thread::spawn(move || {
+            let mut total = 0;
+            while let Ok(report) = copy.pop() {
+                total += report.count_options(0, 0) as u64;
+            }
+            total
+        }));
+    }
+
+    let copy = Arc::clone(&queue_rc);
+
+    let mut last = 0;
+    while copy.len() > 0 {
+        let count = copy.len();
+        if count != last {
+            println!("Left: {}", count);
+            last = count;
+        }
+        thread::sleep(Duration::from_secs(1));
+    }
+
+    let total = handles.into_iter()
+        .map(|handle| handle.join().unwrap())
+        .sum::<u64>();
+    
+    println!("Part 2: {}", total);
 }
 
 fn main() {
     part_one("input.txt");
-    part_two("sample.txt");
+    part_two("input.txt");
 
     println!("Done!");
 }
