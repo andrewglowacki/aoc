fddaf39266310494fc7e484d5336b3b609a1eaec
andrewglowacki <>
2023-12-15 01:56:49 -0500

    day13 more

--- 2023/day13/src/main.rs
+++ 2023/day13/src/main.rs
@@ -60,46 +60,103 @@ impl Map {
         Map { rows, columns }
     }
 
-    fn summarize(&self) -> u32 {
+    fn find_mirror_flip(&mut self, x: usize, y: usize, orig_summary: usize) -> usize {
+        //  ><
+        // xxxxxx 0
+        // xxxxxx 1
+        // 012345 2 
+        // xxxxxx 3
+        // xxxxxx 4
+        //
+        // row_len = 6
+        // col_len = 5
+        // x = 1
+        // y = 2
+        // row_mask = 1 << 4 = 010000
+        // col_mask = 1 << 2 = 000100
+        //
+        let row_mask = 1 << ((self.rows.len() - x) - 1);
+        let col_mask = 1 << ((self.columns.len() - y) - 1);
+
+        let orig_row = self.rows[y];
+        let orig_col = self.columns[x];
+
+        self.rows[y] = orig_row ^ row_mask;
+        self.columns[x] = orig_col ^ col_mask;
+
+        let summary = self.summarize();
+        if summary > 0 && summary != orig_summary {
+            return summary;
+        }
+
+        self.rows[y] = orig_row;
+        self.columns[x] = orig_col;
+
+        return 0;
+    }
+
+    fn find_mirror_flip_summary(&mut self) -> usize {
+        let orig_summary = self.summarize();
+
+        if let Some(column) = Map::find_mirror(&self.columns) {
+            for y in 0..self.rows.len() {
+                let summary = self.find_mirror_flip(column, y, orig_summary);
+                if summary > 0 {
+                    return summary;
+                }
+                let summary = self.find_mirror_flip(column + 1, y, orig_summary);
+                if summary > 0 {
+                    return summary;
+                }
+            }
+            panic!("Alternate not found for original: {} with column mirror: {}!", orig_summary, column);
+        } else if let Some(row) = Map::find_mirror(&self.rows) {
+            for x in 0..self.columns.len() {
+                let summary = self.find_mirror_flip(x, row, orig_summary);
+                if summary > 0 {
+                    return summary;
+                }
+                let summary = self.find_mirror_flip(x, row + 1, orig_summary);
+                if summary > 0 {
+                    return summary;
+                }
+            }
+            panic!("Alternate not found for original: {} with row mirror: {}!", orig_summary, row);
+        } else {
+            panic!("No base mirror found!");
+        }
+    }
+
+    fn summarize(&self) -> usize {
         let column_sum = Map::find_mirror(&self.columns)
-            .map(|index| index as u32 + 1)
+            .map(|index| index + 1)
             .unwrap_or(0);
-        // println!("column_sum is {}", column_sum);
         let row_sum = Map::find_mirror(&self.rows)
-            .map(|index| (index as u32 + 1) * 100)
+            .map(|index| (index + 1) * 100)
             .unwrap_or(0);
-        // println!("row_sum is {}", row_sum);
         column_sum + row_sum
     }
 
     fn find_mirror(within: &Vec<u32>) -> Option<usize> {
         let mid = within.len() / 2;
 
-        // println!("mid={} ", mid);
-        // println!("will test i={:?}", (1..mid).collect::<Vec<_>>());
         let found = (0..mid).rev()
             .filter(|i| within[*i] == within[i + 1])
             .find(|i| {
                 let i = *i;
                 let limit = (i + 1).min((within.len() - i) - 1);
-                // println!("checking i={}, limit={}", i, limit);
-                // (1..limit).for_each(|c| println!("compare {} vs {}", within[i - c], within[i + c + 1]));
                 (1..limit).all(|c| within[i - c] == within[i + c + 1])
             });
         
         if found.is_some() {
             return found;
         }
-        // println!("not found yet");
-        // println!("will test i={:?}", (mid..within.len() - 1).collect::<Vec<_>>());
 
         (mid..within.len() - 1)
             .filter(|i| within[*i] == within[i + 1])
             .find(|i| {
                 let i = *i;
                 let limit = (i + 1).min((within.len() - i) - 1);
-                // println!("checking i={}, limit={}", i, limit);
-                // (1..limit).for_each(|c| println!("compare {} vs {}", within[i - c], within[i + c + 1]));
                 (1..limit).all(|c| within[i - c] == within[i + c + 1])
             })
     }
@@ -124,24 +181,18 @@ fn parse_maps(file_name: &str) -> Vec<Map> {
 
 fn part_one(file_name: &str) {
     let maps = parse_maps(file_name);
-    // let mut i = 0;
     let total = maps.iter()
-        // .inspect(|map| {
-        //     println!("");
-        //     println!("Map {}", i + 1);
-        //     i += 1;
-        //     map.lines.iter().for_each(|line| println!("{}", line));
-        // })
         .map(|map| map.summarize())
-        .sum::<u32>();
+        .sum::<usize>();
     println!("Part 1: {}", total);
 }
 
 fn part_two(file_name: &str) {
-    let lines = get_file_lines(file_name)
-        .flat_map(|line| line.ok());
-    
-    println!("Part 2: {}", "incomplete");
+    let mut maps = parse_maps(file_name);
+    let total = maps.iter_mut()
+        .map(|map| map.find_mirror_flip_summary())
+        .sum::<usize>();
+    println!("Part 2: {}", total);
 }
 
 fn main() {
