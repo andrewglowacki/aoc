b88830f0e4b25f296350edb08889369cb8dab8ee
andrewglowacki <>
2023-12-23 04:01:29 -0500

    day22 pt1.2

--- 2023/day22/src/main.rs
+++ 2023/day22/src/main.rs
@@ -1,6 +1,7 @@
 use core::num;
 use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
 use std::fs::File;
+use std::iter::FromIterator;
 use std::mem::swap;
 use std::path::Path;
 use std::io::{BufRead, BufReader, Lines};
@@ -13,6 +14,7 @@ fn get_file_lines(file_name: &str) -> Input {
     BufReader::new(file).lines()
 }
 
+#[derive(Clone)]
 struct Range {
     from: i32,
     to: i32
@@ -35,6 +37,7 @@ impl Range {
     }
 }
 
+#[derive(Clone)]
 struct Block {
     x: Range,
     y: Range,
@@ -111,10 +114,9 @@ impl BlockPile {
         self.blocks.push(block);
     }
 
-    fn drop_blocks(&mut self) {
+    fn drop_blocks(&mut self, from_z: i32) {
         let z_last = *self.z_to_block.keys().last().unwrap(); 
-        let mut new_blocks = Vec::<usize>::new();
-        for z in 2..z_last {
+        for z in from_z..z_last + 1 {
             if let Some(mut blocks) = self.z_to_block.remove(&z) {
                 while let Some(block_id) = blocks.pop() {
                     let points = &self.block_to_points[block_id];
@@ -125,20 +127,18 @@ impl BlockPile {
                         .filter(|test_z| *test_z < z)
                         .max()
                         .unwrap_or(0);
-                    let new_z_bottom = below + 1;
-                    if new_z_bottom < z {
-                        if let Some(blocks) = self.z_to_block.get_mut(&new_z_bottom) {
-                            blocks.push(block_id);
-                        } else {
-                            self.z_to_block.insert(new_z_bottom, vec![block_id]);
-                        }
-
-                        self.blocks[block_id].z.new_from(new_z_bottom);
-                    } else {
-                        new_blocks.push(block_id);
+                    let mut new_z_bottom = below + 1;
+                    if new_z_bottom >= z {
+                         new_z_bottom = z;
                     }
+                    if let Some(blocks) = self.z_to_block.get_mut(&new_z_bottom) {
+                        blocks.push(block_id);
+                    } else {
+                        self.z_to_block.insert(new_z_bottom, vec![block_id]);
+                    }
+
+                    self.blocks[block_id].z.new_from(new_z_bottom);
                 }
-                swap(&mut new_blocks, &mut blocks);
             }
         }
     }
@@ -150,42 +150,152 @@ impl BlockPile {
             .unwrap();
 
         let mut z_top_to_blocks = Vec::new();
+        let mut z_bottom_to_blocks = Vec::new();
         for _ in 0..max + 1 {
-            z_top_to_blocks.push(Vec::<usize>::new());
+            z_top_to_blocks.push(HashSet::<usize>::new());
+            z_bottom_to_blocks.push(HashSet::<usize>::new());
         }
 
         for block_id in 0..self.blocks.len() {
             let top = self.blocks[block_id].z.to;
-            z_top_to_blocks[top as usize].push(block_id);
+            let bottom = self.blocks[block_id].z.from;
+            z_top_to_blocks[top as usize].insert(block_id);
+            z_bottom_to_blocks[bottom as usize].insert(block_id);
         }
 
-        let mut supported_by = HashMap::<usize, Vec<usize>>::new();
+        let mut key_is_supported_by = HashMap::<usize, Vec<usize>>::new();
         
-        for z in 2..z_top_to_blocks.len() {
-            if let Some(blocks) = self.z_to_block.get(&(z as i32)) {
+        for (z, blocks) in self.z_to_block.range(2..) {
+            let below = &z_top_to_blocks[(*z as usize) - 1];
+            for block in blocks {
+                let supporting = self.block_to_points[*block].iter()
+                    .flat_map(|point| self.xy_to_block.get(point).unwrap())
+                    .filter(|block_id| below.contains(block_id))
+                    .copied()
+                    .collect::<HashSet<_>>();
+                
+                (&mut key_is_supported_by).insert(*block, Vec::from_iter(supporting.into_iter()));
             }
         }
 
-        0
+        let mut key_is_supporting = HashMap::<usize, Vec<usize>>::new();
+
+        for (above, below_blocks) in key_is_supported_by.iter() {
+            for below in below_blocks {
+                if let Some(list) = key_is_supporting.get_mut(&below) {
+                    list.push(*above);
+                } else {
+                    key_is_supporting.insert(*below, vec![*above]);
+                }
+            }
+        }
+
+        let mut count = 0;
+
+        for i in 0..self.blocks.len() {
+            if let Some(supporting) = key_is_supporting.get(&i) {
+                let min_above_supports = supporting.iter()
+                    .map(|block| key_is_supported_by.get(block).unwrap().len())
+                    .min()
+                    .unwrap();
+                if min_above_supports > 1 {
+                    println!("Block {} is dissolvable with supporting: {:?} and min above: {}", i, supporting, min_above_supports);
+                    count += 1;
+                }
+            } else {
+                println!("Block {} is dissolvable not supporting anything", i);
+                count += 1;
+            }
+        }
+
+        count
+    }
+    
+    fn sum_fallen_if_dissolved(&self) -> usize {
+        let max = self.blocks.iter()
+            .map(|block| block.z.to)
+            .max()
+            .unwrap();
+
+        let mut z_top_to_blocks = Vec::new();
+        let mut z_bottom_to_blocks = Vec::new();
+        for _ in 0..max + 1 {
+            z_top_to_blocks.push(HashSet::<usize>::new());
+            z_bottom_to_blocks.push(HashSet::<usize>::new());
+        }
+
+        for block_id in 0..self.blocks.len() {
+            let top = self.blocks[block_id].z.to;
+            let bottom = self.blocks[block_id].z.from;
+            z_top_to_blocks[top as usize].insert(block_id);
+            z_bottom_to_blocks[bottom as usize].insert(block_id);
+        }
+
+        let mut key_is_supported_by = HashMap::<usize, Vec<usize>>::new();
+        
+        for (z, blocks) in self.z_to_block.range(2..) {
+            let below = &z_top_to_blocks[(*z as usize) - 1];
+            for block in blocks {
+                let supporting = self.block_to_points[*block].iter()
+                    .flat_map(|point| self.xy_to_block.get(point).unwrap())
+                    .filter(|block_id| below.contains(block_id))
+                    .copied()
+                    .collect::<HashSet<_>>();
+                
+                (&mut key_is_supported_by).insert(*block, Vec::from_iter(supporting.into_iter()));
+            }
+        }
+
+        let mut key_is_supporting = HashMap::<usize, Vec<usize>>::new();
+
+        for (above, below_blocks) in key_is_supported_by.iter() {
+            for below in below_blocks {
+                if let Some(list) = key_is_supporting.get_mut(&below) {
+                    list.push(*above);
+                } else {
+                    key_is_supporting.insert(*below, vec![*above]);
+                }
+            }
+        }
+
+        let mut count = 0;
+
+        for i in 0..self.blocks.len() {
+            let mut to_check = Vec::new();
+            to_check.push(i);
+            while let Some(check_block) = to_check.pop() {
+                if let Some(supporting) = key_is_supporting.get(&check_block) {
+                    let to_fall = supporting.iter()
+                        .filter(|block| key_is_supported_by.get(block).unwrap().len() == 1)
+                        .collect::<Vec<_>>();
+
+                    count += to_fall.len();
+
+                    to_fall.into_iter().for_each(|block| to_check.push(*block));
+                }
+            }
+        }
+
+        count
     }
 }
 
 fn part_one(file_name: &str) {
     let mut pile = BlockPile::parse(file_name);
-    pile.drop_blocks();
+    pile.drop_blocks(2);
     let count = pile.count_independent();
     println!("Part 1: {}", count);
 }
 
 fn part_two(file_name: &str) {
-    let lines = get_file_lines(file_name)
-        .flat_map(|line| line.ok());
-    
-    println!("Part 2: {}", "incomplete");
+    let mut pile = BlockPile::parse(file_name);
+    pile.drop_blocks(2);
+    let total = pile.sum_fallen_if_dissolved();
+    println!("Part 2: {}", total);
 }
 
 fn main() {
-    part_one("sample.txt");
+    part_one("input.txt");
     part_two("sample.txt");
 
     println!("Done!");
