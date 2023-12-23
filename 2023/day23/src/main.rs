9d13e4d934c22e525f93585f081019552fb8784f
andrewglowacki <>
2023-12-23 02:33:07 -0500

    day23 part2

--- 2023/day23/src/main.rs
+++ 2023/day23/src/main.rs
@@ -15,12 +15,52 @@ fn get_file_lines(file_name: &str) -> Input {
 
 struct Forest {
     trails: HashMap<(i32, i32), (i32, i32)>,
-    end: (i32, i32),
-    sloped: bool
+    end: (i32, i32)
+}
+
+struct Visited {
+    set: HashSet<(i32, i32)>,
+    list: Vec<(i32, i32)>,
+    longest: usize
+}
+
+impl Visited {
+    fn new() -> Visited {
+        Visited {
+            set: HashSet::new(),
+            list: Vec::new(),
+            longest: 0
+        }
+    }
+    fn len(&self) -> usize {
+        self.list.len()
+    }
+    fn contains(&self, point: &(i32, i32)) -> bool {
+        self.set.contains(point)
+    }
+    fn reset_to(&mut self, length: usize) {
+        let diff = self.list.len() - length;
+        for _ in 0..diff {
+            let point = self.list.pop().unwrap();
+            self.set.remove(&point);
+        }
+    }
+    fn add(&mut self, point: (i32, i32)) {
+        if self.set.insert(point) {
+            self.list.push(point);
+        }
+    }
+    fn update_longest(&mut self) {
+        let current = self.list.len();
+        if current > self.longest {
+            self.longest = current;
+            println!("Longest is now {}", current);
+        }
+    }
 }
 
 impl Forest {
-    fn parse(file_name: &str, sloped: bool) -> Forest {
+    fn parse(file_name: &str) -> Forest {
         let mut y = 0;
         let trails = get_file_lines(file_name)
             .flat_map(|line| line.ok())
@@ -57,12 +97,16 @@ impl Forest {
 
         Forest {
             end,
-            trails,
-            sloped
+            trails
         }
     }
+    
 
-    fn find_longest_trail(&self, mut next: (i32, i32), mut visited: HashSet<(i32, i32)>) -> usize {
+    fn find_longest_trail_slopes(
+        &self, 
+        mut next: (i32, i32), 
+        mut visited: HashSet<(i32, i32)>) -> usize 
+    {
         visited.insert(next);
 
         if next == self.end {
@@ -73,6 +117,7 @@ impl Forest {
         let mut longest = 0;
         let mut new_next_points = Vec::new();
         let mut flat_points = Vec::<((i32, i32), Vec<(i32, i32)>)>::new();
+        let mut branches = Vec::<((i32, i32), Vec<(i32, i32)>, HashSet<(i32, i32)>)>::new();
 
         // loop until thre are new
         loop {
@@ -84,54 +129,53 @@ impl Forest {
                 (x, y + 1)
             ];
 
-            if self.sloped {
-                // out of the possible neighbors,
-                // find ones that are on the trail
-                // and haven't been visited yet
-                let mut next_points = neighbors.into_iter()
-                    .filter(|point| !visited.contains(point))
-                    .filter_map(|point| match self.trails.get(&point) {
-                        Some(slope) => Some((point, slope, Vec::new())),
-                        None => None
-                    })
-                    .collect::<Vec<_>>();
+            // out of the possible neighbors,
+            // find ones that are on the trail
+            // and haven't been visited yet
+            let mut next_points = neighbors.into_iter()
+                .filter(|point| !visited.contains(point))
+                .filter_map(|point| match self.trails.get(&point) {
+                    Some(slope) => Some((point, slope, Vec::new())),
+                    None => None
+                })
+                .collect::<Vec<_>>();
 
-                // println!("From ({}, {}), next points are: {:?}", x, y, next_points);
+            // println!("From ({}, {}), next points are: {:?}", x, y, next_points);
 
-                // for each of the next points, follow any 
-                // sloped trails. If one of these points
-                // isn't on a slope, just add it to the flat_points.
-                while next_points.len() > 0 {
+            // for each of the next points, follow any 
+            // sloped trails. If one of these points
+            // isn't on a slope, just add it to the flat_points.
+            while next_points.len() > 0 {
 
-                    while let Some(((x, y), (slope_x, slope_y), mut followed)) = next_points.pop() {
-                        if *slope_x == 0 && *slope_y == 0 {
-                            flat_points.push(((x, y), followed));
-                        } else {
-                            followed.push((x, y));
-                            let next_point = (x + slope_x, y + slope_y);
-                            if !visited.contains(&next_point) {
-                                if let Some(new_slope) = self.trails.get(&next_point) {
-                                    new_next_points.push((next_point, new_slope, followed));
-                                }
+                while let Some(((x, y), (slope_x, slope_y), mut followed)) = next_points.pop() {
+                    if *slope_x == 0 && *slope_y == 0 {
+                        flat_points.push(((x, y), followed));
+                    } else {
+                        followed.push((x, y));
+                        let next_point = (x + slope_x, y + slope_y);
+                        if !visited.contains(&next_point) {
+                            if let Some(new_slope) = self.trails.get(&next_point) {
+                                new_next_points.push((next_point, new_slope, followed));
                             }
                         }
                     }
-                    
-                    swap(&mut new_next_points, &mut next_points);
                 }
-            } else {
                 
-                neighbors.into_iter()
-                    .filter(|point| !visited.contains(point))
-                    .filter_map(|point| match self.trails.get(&point) {
-                        Some(_) => Some((point, Vec::<(i32, i32)>::new())),
-                        None => None
-                    })
-                    .for_each(|entry| flat_points.push(entry));
+                swap(&mut new_next_points, &mut next_points);
             }
 
             if flat_points.is_empty() {
                 // println!("No flat points left, longest is: {}", longest);
+                for (next, followed, mut visited) in branches {
+                    // println!("Branching with next: {:?}", next);
+                    followed.into_iter().for_each(|point| { 
+                        visited.insert(point); 
+                    });
+                    let this_longest = self.find_longest_trail_slopes(next, visited);
+                    longest = longest.max(this_longest);
+                }
+                
+                
                 return longest;
             } else {
                 // continue on the trail with the next flat point
@@ -142,19 +186,80 @@ impl Forest {
                 // for each of the other possible directions
                 // the trail could go, find the longest trail
                 while let Some((next, followed)) = flat_points.pop() {
-                    // println!("Also following fork with next: {:?}", next);
-                    let mut new_visited = visited.clone();
-                    followed.into_iter().for_each(|point| { 
-                        new_visited.insert(point); 
-                    });
-                    let this_longest = self.find_longest_trail(next, new_visited);
-                    longest = longest.max(this_longest);
+                    branches.push((next, followed, visited.clone()));
                 }
                 
                 followed.into_iter().for_each(|point| { 
                     visited.insert(point); 
                 });
                 visited.insert(next);
+                
+                if next == self.end {
+                    longest = longest.max(visited.len());
+
+                    for (next, followed, mut visited) in branches {
+                        // println!("Branching with next: {:?}", next);
+                        followed.into_iter().for_each(|point| { 
+                            visited.insert(point); 
+                        });
+                        let this_longest = self.find_longest_trail_slopes(next, visited);
+                        longest = longest.max(this_longest);
+                    }
+
+                    return longest;
+                }
+                // println!("Next iteration will be: {:?}", next);
+            }
+        }
+    }
+
+    fn find_longest_trail_no_slopes(
+        &self, 
+        mut next: (i32, i32), 
+        visited: &mut Visited) -> usize 
+    {
+        visited.add(next);
+
+        if next == self.end {
+            // println!("Found the end with size: {}", visited.len());
+            visited.update_longest();
+            return visited.len();
+        }
+
+        let mut longest = 0;
+        let mut flat_points = Vec::<(i32, i32)>::new();
+
+        // loop until thre are new
+        loop {
+            let (x, y) = next;
+            let neighbors = vec![
+                (x - 1, y),
+                (x + 1, y),
+                (x, y - 1),
+                (x, y + 1)
+            ];
+
+            neighbors.into_iter()
+                .filter(|point| !visited.contains(point) && self.trails.contains_key(point))
+                .for_each(|point| flat_points.push(point));
+
+            if flat_points.is_empty() {
+                // println!("No flat points left, longest is: {}", longest);
+                return longest;
+            } else {
+                // continue on the trail with the next flat point
+                next = flat_points.pop().unwrap();
+                
+                // for each of the other possible directions
+                // the trail could go, find the longest trail
+                while let Some(next) = flat_points.pop() {
+                    let visit_len = visited.len();
+                    let this_longest = self.find_longest_trail_no_slopes(next, visited);
+                    visited.reset_to(visit_len);
+                    longest = longest.max(this_longest);
+                }
+                
+                visited.add(next);
                 
                 if next == self.end {
                     // println!("Found the end with visited (count {}): {:?}", visited.len(), visited);
@@ -178,7 +283,11 @@ impl Forest {
                     //     }
                     //     println!("");
                     // }
-                    return longest.max(visited.len());
+                    longest = longest.max(visited.len());
+                    visited.update_longest();
+
+                    // println!("Found end with longest {}", longest);
+                    return longest;
                 }
                 // println!("Next iteration will be: {:?}", next);
             }
@@ -187,14 +296,14 @@ impl Forest {
 }
 
 fn part_one(file_name: &str) {
-    let forest = Forest::parse(file_name, true);
-    let longest_trail = forest.find_longest_trail((1, 0), HashSet::new()) - 1;
+    let forest = Forest::parse(file_name);
+    let longest_trail = forest.find_longest_trail_slopes((1, 0), HashSet::new()) - 1;
     println!("Part 1: {}", longest_trail);
 }
 
 fn part_two(file_name: &str) {
-    let forest = Forest::parse(file_name, false);
-    let longest_trail = forest.find_longest_trail((1, 0), HashSet::new()) - 1;
+    let forest = Forest::parse(file_name);
+    let longest_trail = forest.find_longest_trail_no_slopes((1, 0), &mut Visited::new()) - 1;
     println!("Part 2: {}", longest_trail);
 }
 
